f36a88be3d02470e6fab6fa32143e8228860a25b
Rishika <rishikarocky14@gmail.com>
2021-05-18 23:59:10 +0530

    societies

--- src/pages/link4.jsx
+++ src/pages/link4.jsx
@@ -7,36 +7,86 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout route="/link4">
     <Seo title="Link 4" />
-
-    <div className="pt-10 pb-5 px-2 sm:px-10">
-      <p className="py-72 bg-white rounded-lg shadow-2xl">
-        Content
-        <span role="img" aria-label="partying-face">
-          🥳
-        </span>
-        <span role="img" aria-label="partying-face">
-          🥳
-        </span>
-        <span role="img" aria-label="smiling-face-with-halo">
-          😇
-        </span>
-      </p>
+    {/* 1 */}
+    <div id="soc1" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                       
+                    </div>
     </div>
-
-    <div className="pt-5 pb-10 px-2 sm:px-10">
-      <p className="py-72 bg-white rounded-lg shadow-2xl">
-        Content
-        <span role="img" aria-label="partying-face">
-          🥳
-        </span>
-        <span role="img" aria-label="partying-face">
-          🥳
-        </span>
-        <span role="img" aria-label="smiling-face-with-halo">
-          😇
-        </span>
-      </p>
+    {/* 2 */}
+    <div id="soc2" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                        
+                    </div>
     </div>
+    {/* 3 */}
+    <div id="soc3" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                        
+                    </div>
+                </div>
+    {/* 4 */}
+    <div id="soc4" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                        
+                    </div>
+                </div>
+    {/* 5 */}
+    <div id="soc5" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                        
+                    </div>
+                </div>
+    {/* 6 */}
+    <div id="soc6" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                        
+                    </div>
+                </div>
+    {/* 7 */}
+    <div id="soc7" className="mx-auto mt-10 bg-white  lg:flex  lg:max-w-5xl lg:shadow-lg lg:rounded-lg lg:flex lg:justify-center">
+                    <div className="lg:w-1/2">
+                        <img classname="h-64 bg-cover lg:rounded-lg lg:h-full" src='https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'/>
+                    </div>
+                    <div className="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                        <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                        
+                    </div>
+                </div>
+    
+
+    
   </Layout>
 )
 
