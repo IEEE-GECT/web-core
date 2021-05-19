9f82b6fbfadaa4bf674b751c6cdb37fd71f483ec
Abil Savio <abilsavio@gmail.com>
2021-05-19 19:36:46 +0530

    Add wordsworld

---
+++ src/pages/e/rural-la-carte.jsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+const IndexPage = () => (
+  <Layout route="/e">
+    <Seo title="Rural la carte" />
+
+    <div className="h-screen">
+      <iframe
+        id="typeform-full"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        allow="camera; microphone; autoplay; encrypted-media;"
+        src="https://form.typeform.com/to/yE5vGoaO?typeform-medium=embed-snippet"
+      ></iframe>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
