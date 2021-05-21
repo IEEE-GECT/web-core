cddf29666bf12138052893c3a52887de4580e795
Malayil Vivekanandan <malayil142@gmail.com>
2021-05-21 21:04:41 +0530

    next-deploy

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -142,15 +142,15 @@ const SideBar = ({ route, setIsOpen }) => {
           <NavLink to="/">Home</NavLink>
           <NavLink to="/chapters">Chapters</NavLink>
           <DropDown to="/e/" title="Registrations">
-            <DropDownLink to="/e/rural-la-carte">Rural la carte</DropDownLink>
-            <DropDownLink to="/e/wordsworld">Wordsworld</DropDownLink>
-            <DropDownLink to="/e/kyuriosity">Kyuriosity</DropDownLink>
-            <DropDownLink to="/e/technocrat">Technocrat</DropDownLink>
-            <DropDownLink to="/e/level-up">Level Up</DropDownLink>
-            <DropDownLink to="/e/up-the-ante">Up the Ante</DropDownLink>
             <DropDownLink to="/e/corporate-roadies">
               Corporate Roadies
             </DropDownLink>
+            <DropDownLink to="/e/wordsworld">Wordsworld</DropDownLink>
+            <DropDownLink to="/e/rural-la-carte">Rural la carte</DropDownLink>
+            <DropDownLink to="/e/level-up">Level Up</DropDownLink>
+            <DropDownLink to="/e/up-the-ante">Up the Ante</DropDownLink>
+            <DropDownLink to="/e/technocrat">Technocrat</DropDownLink>
+            <DropDownLink to="/e/kyuriosity">Kyuriosity</DropDownLink>
           </DropDown>
           <NavLink to="/execom">Execom</NavLink>
         </nav>
