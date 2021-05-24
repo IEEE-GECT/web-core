649fc3f722625d171649b9d8f67d491ca3350c4b
Malayil Vivekanandan <malayil142@gmail.com>
2021-05-24 21:41:14 +0530

    meta tags update to include image

--- src/pages/execom.jsx
+++ src/pages/execom.jsx
@@ -6,7 +6,10 @@ import { Team } from "../components/team.component"
 
 const ExecomPage = () => (
   <Layout route="/execom">
-    <Seo title="Execom" />
+    <Seo
+      title="Execom"
+      image="https://8s9ot3736652r3v5s22dfc81-wpengine.netdna-ssl.com/wp-content/uploads/2021/04/Consensus-Kills-Business-Innovation-600x315-cropped.jpg"
+    />
 
     <div className="pt-5 pb-10 px-2 sm:px-10">
       <div className="rounded-lg shadow-2xl glass-effect">
