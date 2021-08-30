aac2d9dd349ae109b5bcdba57d0301a5bd8716e4
Malayil Vivekanandan <malayil142@gmail.com>
2021-08-30 17:38:55 +0530

    AuRea on valaista and Registations

--- src/pages/valaista.jsx
+++ src/pages/valaista.jsx
@@ -272,6 +272,32 @@ const Valaista = () => (
 
                 <div className="mb-8 flex justify-between items-center w-full right-timeline">
                   <div className="order-1 w-5/12 hidden sm:block"></div>
+                  <div className="order-1 w-full sm:w-5/12 px-1 py-4 text-left ml-1 sm:ml-0">
+                    <p className="mb-3 text-base text-yellow-300">
+                      Sept 4, 7pm - 8pm
+                    </p>
+                    <h4 className="mb-3 font-bold text-lg md:text-2xl">
+                      AuRea - Doorstep to AR
+                    </h4>
+                    <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100 mb-4">
+                      WORKSHOP CONTENT <br></br>⚜️ Introduction to Augmented
+                      Reality and virtual reality<br></br> ⚜️ AR Devices and VR
+                      Devices <br></br>⚜️Development platforms <br></br>⚜️
+                      Augmented Reality Industrial application ⚜️Demonstration
+                      of AR
+                    </p>
+                    <Link
+                      to="/e/aurea"
+                      className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
+                      style={{ maxWidth: "130px" }}
+                    >
+                      Register
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="mb-8 flex justify-between items-center w-full right-timeline">
+                  <div className="order-1 w-5/12 hidden sm:block"></div>
 
                   <div className="order-1 w-full sm:w-5/12 px-1 py-4">
                     <p className="mb-3 text-base text-yellow-300"></p>
