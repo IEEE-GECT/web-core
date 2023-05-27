312567fbcb25ede0db28859186cbcacd52630684
anuragck-ui <anuragck22@gmail.com>
2023-05-27 09:09:57 +0530

    Updates

---
+++ component---src-pages-register-jsx-eff67d0a42183fc497fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkieee_gect=self.webpackChunkieee_gect||[]).push([[858],{8266:function(e,t,r){r.r(t);var c=r(7294),n=r(9018),i=r(5869),s=r(2059);t.default=()=>{const e=c.useRef();return c.useEffect((()=>{(0,n.createWidget)("qAxpT6jn",{container:e.current})}),[]),c.createElement(i.Z,{route:"/register"},c.createElement(s.Z,{title:"IEEE Membership Form",description:"Join IEEE Now!"}),c.createElement("div",{style:{height:"calc(var(--vh, 1vh) * 100 - 5rem )"},ref:e}))}}}]);
+//# sourceMappingURL=component---src-pages-register-jsx-eff67d0a42183fc497fc.js.map
