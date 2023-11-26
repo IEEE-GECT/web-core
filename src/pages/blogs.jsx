e52cd40dcfaf2c9d69f4516401cabb858517ec2b
Ben-A-Sabu <benasabu123@gmail.com>
2023-11-26 09:40:50 +0530

    blog added

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -43,6 +43,14 @@ let blogposts = [
       "https://ik.imagekit.io/nm1ondc9y/aditya_l1.jpg?updatedAt=1698661705900",
     link: "/blogs/aditya_l1",
   },
+  {
+    title: "Microbial DNA: Decoding the Subterranean Saga",
+    description: `In the silent realms of scientific discovery, Microbial DNA stands as Earth's virtuoso composer, crafting a symphony with ancient whispers from subterranean life. 
+    Can it unravel the enigmatic saga beneath our feet, revealing secrets as astonishing as the wonders hidden in the depths of our planet? `,
+    image:
+      "https://ik.imagekit.io/nm1ondc9y/MICROBIALDNA.jpg?updatedAt=1700969644062",
+    link: "/blogs/MicrobialDNA",
+  },
 ]
 
 const BlogPage = () => (
